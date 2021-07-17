8346b91a8a3c4ab8933bb0af4eec9fd0e62a6842
Sathsara18854 <rsathsara330@gmail.com>
2021-07-17 14:30:54 +0530

    This is Sum class

---
+++ Sum.java
@@ -0,0 +1,9 @@
+public class Sum{
+    int num1 = 12;
+    int num2 = 45;
+    int total = num1+ num2;
+    
+    public static void add(){
+        System.out.println("Total is " + total);
+    }
+}
