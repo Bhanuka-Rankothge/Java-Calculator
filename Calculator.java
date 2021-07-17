4547dbd5d322a0290ea24dc28a7a1f9ee89b6cc5
Bhanuka-Rankothge <86610640+Bhanuka-Rankothge@users.noreply.github.com>
2021-07-17 16:09:56 +0530

    Added a scanner object to the program
    Please edit your variables as number1,number2,opereator

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Calculator{
   public static void main(){
+    Scanner scanNumber = new Scannner(System.in);
+    
+    System.out.print("Number 1 : ");
+    number1 = scanNumber.nextFloat.nextLine();
+    
+    System.out.print("Operator : ");
+    oprator = scanNumber.nextLine();
+    
+    System.out.print("Number 2 : ");
+    number2 = scanNumber.nextFloat.nextLine();
     
   }
 }
